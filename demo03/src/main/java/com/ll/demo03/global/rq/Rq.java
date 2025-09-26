88cf2c4665fd6f6356c159b7a9745d3c67745694
leeyumin626 <leeym7404@naver.com>
2025-09-27 04:19:00 +0900

    9장

--- demo03/src/main/java/com/ll/demo03/global/rq/Rq.java
+++ demo03/src/main/java/com/ll/demo03/global/rq/Rq.java
@@ -1,18 +1,47 @@
+
 package com.ll.demo03.global.rq;
 
 import com.ll.demo03.domain.member.member.entity.Member;
 import com.ll.demo03.domain.member.member.service.MemberService;
+import com.ll.demo03.global.exceptions.GlobalException;
+import com.ll.demo03.standard.dto.util.Ut;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
 @Component
-@RequestScope // Rq의 객체가 요청이 들어올 때마다 하나씩 생김
+@RequestScope
 @RequiredArgsConstructor
 public class Rq {
+    private final HttpServletRequest req;
+    private final HttpServletResponse resp;
     private final MemberService memberService;
+    private Member member;
 
     public Member getMember() {
-        return memberService.getReferenceById(1L);
+        if (member != null) return member;
+
+        String actorUsername = req.getParameter("actorUsername");
+        String actorPassword = req.getParameter("actorPassword");
+
+        if (Ut.str.isBlank(actorUsername)) throw new GlobalException("401-1", "인증정보(아이디)를 입력해주세요.");
+        if (Ut.str.isBlank(actorPassword)) throw new GlobalException("401-2", "인증정보(비밀번호)를 입력해주세요.");
+
+        Member loginedMember = memberService.findByUsername(actorUsername).orElseThrow(() -> new GlobalException("403-3", "해당 회원이 존재하지 않습니다."));
+        if (!loginedMember.getPassword().equals(actorPassword)) throw new GlobalException("403-4", "비밀번호가 일치하지 않습니다.");
+
+        member = loginedMember;
+
+        return loginedMember;
+    }
+
+    public String getCurrentUrlPath() {
+        return req.getRequestURI();
+    }
+
+    public void setStatusCode(int statusCode) {
+        resp.setStatus(statusCode);
     }
 }
